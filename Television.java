61bbea37a6e2f13fc7c712d15ee5f97f66630514
Jonathan Taylor <joncody2012@gmail.com>
2016-02-09 09:27:02 -0600

    Task #1
    Cannot find symbols for MANUFACTURER or SCREEN_SIZE

---
+++ Television.java
@@ -0,0 +1,22 @@
+// The purpose of this class is to model a television
+// Jonathan Taylor 2/9/2016
+
+public class Television
+{
+        	
+                        
+        boolean powerOn;                //Determines the power of the TV
+        int channel;                    //Determines channel of TV  
+        int volume;                     //Determines volume of TV
+       
+		                     //Determines volume of TV
+            public Television()
+             {
+                MANUFACTURER = "Vizio";       //The manufacturer of the TV
+                SCREEN_SIZE = 32;             //Size of the TV
+             }	
+
+	
+}
+    
+
